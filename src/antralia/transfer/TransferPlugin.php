44b40c66d3199131dfc8fcaafe371c69046a30c2
Artem Turov <58974140+Lunarelly@users.noreply.github.com>
2022-11-29 23:56:19 +0300

    v1

---
+++ src/antralia/transfer/TransferPlugin.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ *     _          _             _ _
+ *    / \   _ __ | |_ _ __ __ _| (_) __ _
+ *   / _ \ | '_ \| __| '__/ _` | | |/ _` |
+ *  / ___ \| | | | |_| | | (_| | | | (_| |
+ * /_/   \_\_| |_|\__|_|  \__,_|_|_|\__,_|
+ *
+ * @author Antralia (Lunarelly)
+ * @link https://github.com/Antralia
+ *
+ */
+
+namespace antralia\transfer;
+
+use pocketmine\event\EventPriority;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginException;
+
+use ReflectionException;
+
+final class TransferPlugin extends PluginBase
+{
+
+    /**
+     * @return void
+     */
+    protected function onEnable(): void
+    {
+        try {
+            $this->getServer()->getPluginManager()->registerEvent(PlayerJoinEvent::class, function (PlayerJoinEvent $event): void {
+                $event->getPlayer()->transfer("antralia.net", 19132, "Transferring to Antralia Network");
+            }, EventPriority::MONITOR, $this);
+        } catch (ReflectionException $exception) {
+            throw new PluginException($exception->getMessage());
+        }
+    }
+}
